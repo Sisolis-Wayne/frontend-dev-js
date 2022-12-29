3682e55a545487dfae2f5925bceda8ebad128784
Sisolis Wayne <mohammedakoredest@gmail.com>
2022-12-29 04:59:53 +0100

    added a new html file

---
+++ assignment2.js
@@ -0,0 +1,12 @@
+// console.log('mine');
+
+let btn = document.querySelector('#darkMode')
+// btn.innerHTML = 'CHANGE'
+btn.addEventListener('click', function () {
+    console.log('milk')
+    btn.innerHTML = `<button style="background: blue; color: white">CHAN</button>`
+})
+
+
+
+
